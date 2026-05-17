8ec1d4d73103478593b1318840f62778980678f1
mari-ee <mariahmakeda@gmail.com>
2026-05-16 23:25:43 -0500

    style: redesign about + edu sections, shrink navbar
    - reworked about me card layout and colors
    - updated education card with tilted sticker labels
    - reduced menu tab size

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 const MenuBar = () => {
   return (
       <div className={"menu-bar"}>
-        <img src={logo} alt="logo" width="270"/>
+        <img src={logo} alt="logo" width="170"/>
           <nav className={"all-nav-buttons"}>
           <MenuButton name={"ABOUT"} loc={"about"}/>
           <MenuButton name={"EDUCATION"} loc={"edu"}/>
@@ -30,32 +30,37 @@ const MenuButton = ({name, loc}) => {
 const About = () => {
     return (
         <div className={"about"}>
-            <div>
-            <h3 className={"main-header"}>ABOUT ME</h3>
-            <p className={"about-passage"}>Hello there! My name is Mariah Makeda Ogunlana, a Biomedical Science student minoring in Computer Science. I enjoy learning how to build applications, working with data, and solving problems through code. I’m especially interested in how technology can be used to create meaningful real-world impact. I am a huge Stray Kids lover (STAY here!!) and in my free time, I like to read, watch Chinese dramas, and listen to music.
-                <b>Fun fact:</b> If you ever meet me, I’m probably listening to something :) </p>
+            <h3 className={"header"}>ABOUT ME</h3>
+            <div className={"about-content"}>
+                <img src={profPic} alt="professional-picture" className={"prof-pic"} />
+            <p className={"about-passage"}>Hello there! My name is Mariah Makeda Ogunlana, a Biomedical Science student minoring in Computer Science. I enjoy learning how to build applications, working with data, and solving problems through code.
+                I’m especially interested in how technology can be used to create meaningful real-world impact. I am a huge Stray Kids lover (STAY here!!) and in my free time, I like to read Christian fiction, and learn Chinese and French.
+                <br/> Also, if you ever meet me in public, I’m probably listening to something :) </p>
             </div>
-            <img className={"prof-pic"} src={profPic} alt="professional-picture"/>
         </div>
     )
 }
 const Education = () => {
     return (
     <div className={"edu"}>
-        <div className={"main-header"}>EDUCATION</div>
+        <div className={"header"}>EDUCATION</div>
         <div className={"edu-content"}>
             <div className={"edu-box"}>
-                <h4 className={"edu-1-sch-name"}>University of Texas at Dallas</h4>
-                <p className={"edu-1-sch-degree"}>Bachlor of Science</p>
-                <hr className={"edu-1-sch-line"}/>
-                <p className={"edu-1-sch-major"}>Biomedical Science</p>
-                <p className={"edu-1-sch-minor"}>Computer Science</p>
+                <h4 className={"edu-sch-name"}>University of Texas at Dallas (B.Sc)</h4>
+                <p className={"edu-sch-years"}> Sept. 2025 - Present</p>
+                <hr className={"edu-dividing-line"}/>
 
+                <p className={"edu-major-text"}>
+                    <span className={"edu-major-title"}>Major:</span> Biomedical Science</p>
+                <p className={"edu-minor-text"}>
+                    <span className={"edu-minor-title"}>Minor:</span> Computer Science</p>
             </div>
 
             <div className={"edu-box"}>
-                <h4>Collin College<br/></h4>
-                <p>Associate of Science</p>
+                <h4 className={"edu-sch-name"}>Collin County Community College</h4>
+                <p className={"edu-sch-years"}>Jan. 2024 - May 2025</p>
+                 <hr className={"edu-dividing-line"}/>
+                <p>Associate of Science in <span className={"edu-major-text"}>Computer Science</span> </p>
             </div>
         </div>
     </div>
