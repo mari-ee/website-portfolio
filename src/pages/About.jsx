59a1e11a2b2235b5d6b70fbd6961fc36a073a2de
mari-ee <mariahmakeda@gmail.com>
2026-06-15 08:36:15 -0500

    Update name details in Hero and About sections

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
             <div className="about-content">
                 <img src={profPic} alt="professional-picture" className="prof-pic" />
                 <p className="about-passage">
-                    Hello there! My name is (Mariah) Makeda Ogunlana, a Biomedical Science student minoring in Computer Science.
+                    Hello there! My name is Makeda Ogunlana, a Biomedical Science student minoring in Computer Science.
                     I enjoy learning how to build applications, working with data, and solving problems through code.
                     I'm especially interested in how technology can be used to create meaningful real-world impact.<br/>
                     I am a huge Stray Kids lover (STAY here!!) and in my free time, I like to read Christian fiction,
